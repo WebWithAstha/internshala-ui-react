e63f91003064ff17ce68f7f406ae870b6a049361
WebWithAstha <lodhiastha85@gmail.com>
2024-08-04 19:10:16 +0530

    student signup done

--- src/components/student/SignUp.jsx
+++ src/components/student/SignUp.jsx
@@ -1,32 +1,44 @@
-import { React, useEffect } from 'react'
+import { React, useEffect, useState } from 'react'
 import Nav from '../partials/navbar/Nav.jsx'
 import LogDetails from '../partials/LogDetails'
 import axios from '../../utils/axios.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
 
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const [firstname, setfirstname] = useState('')
+  const [lastname, setlastname] = useState('')
+  const [contact, setcontact] = useState('')
+  const navigate = useNavigate()
+
+
   const studentSignUp = async () => {
     try {
-      const data = await axios.get('/student', {
-        password: "123456",
-        email: "lodhiastha@gmail.com",
+      const {data} = await axios.post('/student/signup', {
+        firstname,
+        lastname,
+        email,
+        password,
+        contact,
         gender: "Female",
         city: "bhopal",
-        contact: "1234567890",
-        firstname: "John",
-        lastname: "Smith"
       })
-      console.log(data.data)
+      console.log(data)
+      navigate('/student/dashboard',{state:{id:data.id}})
     } catch (error) {
+      alert(error.response.data.message)
       console.log(error.response.data.message)
     }
 
-
   }
-  useEffect(() => {
-    studentSignUp()
 
-  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    studentSignUp()
+  }
 
 
   return (
@@ -39,7 +51,7 @@ const SignUp = () => {
           <h2 className='text-center text-[1.6rem] font-normal mt-4'>1,50,000+ companies hiring on Internshala</h2>
         </div >
 
-        <form action="" className='max-w-[32rem] w-[30rem] shadow-md rounded-lg bg-white px-8 py-8'>
+        <form onSubmit={e=>handleSubmit(e)} action="" className='max-w-[32rem] w-[30rem] shadow-md rounded-lg bg-white px-8 py-8'>
 
           <button className='flex items-center gap-2 rounded text-center w-full justify-center border border-zinc-300 mt-6 py-2 font-semibold'><img className='w-4' src="https://cdn-icons-png.flaticon.com/128/300/300221.png" alt="" /> Sign Up with Google</button>
           <div className="flex w-full gap-2 mt-4 justify-center items-center">
@@ -47,7 +59,19 @@ const SignUp = () => {
             <span>OR</span>
             <hr className='h-[.08rem] w-full bg-zinc-200' />
           </div>
-          <LogDetails btn={"Sign Up"} />
+          <LogDetails 
+          btn={"Sign Up"} 
+          email={email}
+          setemail={setemail} 
+          password={password}
+          setpassword={setpassword} 
+          firstname={firstname}
+          setfirstname={setfirstname} 
+          lastname={lastname} 
+          setlastname={setlastname} 
+          contact={contact} 
+          setcontact={setcontact} 
+          />
         </form>
 
 
