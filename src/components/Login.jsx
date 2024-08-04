bb7d4bbd48a3ff764d868b3938937da99ae3d71b
WebWithAstha <lodhiastha85@gmail.com>
2024-08-04 19:10:30 +0530

    student login done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from "../utils/axios";
 
 const Login = () => {
   const navigate = useNavigate()
 
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+
+
+  const sendLoginDetails = async()=>{
+    try {
+      const {data} = await axios.post('/student/signin', {
+        email,
+        password,
+      });
+      console.log(data)
+      navigate('/student/dashboard',{state: {id:data.id} })
+    } catch (error) {
+      console.error(error.response.data.message);
+      alert(error.response.data.message);
+    }
+  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    sendLoginDetails()
+
+  }
+
+
+
+
   return (
       <div className="w-full h-full fixed top-0 left-0 bg-black/[.8] z-[999] flex justify-center items-center">
 
-        <form action="" className='max-w-[28rem] bg-white px-8 py-8'>
+        <form onSubmit={e=>handleSubmit(e)} action="" className='max-w-[28rem] bg-white px-8 py-8'>
         
         <i onClick={e=>navigate(-1)} className="fa-solid fa-xmark relative top-0 left-[93%] text-xl mb-2"></i>
         <div className="flex w-full gap-2">
@@ -20,10 +49,11 @@ const Login = () => {
                 <span>OR</span>
                 <hr className='h-[.08rem] w-full bg-zinc-200' />
             </div>
+
             <label htmlFor="email" className='mt-4 inline-block text-lg'>Email</label>
-            <input type="email" className='w-full outline-sky-500 rounded border border-zinc-300 px-2 py-2 mt-1' placeholder='john@example.com' />
+            <input required onChange={e=>setemail(e.target.value)} value={email} type="email" className='w-full outline-sky-500 rounded border border-zinc-300 px-2 py-2 mt-1' placeholder='john@example.com' />
             <label htmlFor="password" className='mt-4 inline-block text-lg'>Password</label>
-            <input type="password" className='w-full outline-sky-500 rounded border border-zinc-300 px-2 py-2 mt-1' placeholder='john@example.com' />
+            <input required onChange={e=>setpassword(e.target.value)} value={password} minLength={6} maxLength={12} type="password" className='w-full outline-sky-500 rounded border border-zinc-300 px-2 py-2 mt-1' placeholder='john@example.com' />
             <h4 className="ml-auto text-sky-600 text-right mt-5 font-semibold">Forgot Password?</h4>
             <button className='px-6 w-full bg-sky-500 hover:bg-sky-400 duration-300 font-bold text-white py-[.5rem] rounded mt-6'>Login</button>
             <h4 className='mt-6 text-center font-medium text-zinc-500'>New to Internshala? Register (<span className='text-sky-600'>Student</span> / <span className='text-sky-600'>Company</span>)</h4>
