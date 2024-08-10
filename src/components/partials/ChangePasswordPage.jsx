33adfcd40a5d3d7c8ec33890eeaeeb7d42d35d93
WebWithAstha <lodhiastha85@gmail.com>
2024-08-10 21:11:00 +0530

    employee reset password done

--- src/components/partials/ChangePasswordPage.jsx
+++ src/components/partials/ChangePasswordPage.jsx
@@ -1,52 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { asyncLoad } from '../../store/actions/studentAction';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { asyncLoad as studentAsyncLoad } from '../../store/actions/studentAction';
+import { asyncLoad as employeeAsyncLoad } from '../../store/actions/employeeAction';
 import Nav from './navbar/Nav.jsx';
 import axios from '../../utils/axios.jsx'
 
 const ChangePasswordForm = () => {
 
-    const navigate= useNavigate()
-    const dispatch = useDispatch()
-    const { info } = useSelector(store => store.studentReducer)
-  
-    useEffect(() => {
-      dispatch(asyncLoad(navigate))
-    }, [])
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { state } = useLocation()
+  const { info } = useSelector(store => 
+    state && state.userType === 'student' 
+      ? store.studentReducer 
+      : store.employeeReducer
+  );
+  useEffect(() => {
+    const asyncLoad = state.userType === 'student' ? studentAsyncLoad : employeeAsyncLoad;
+    dispatch(asyncLoad(navigate))
+  }, [])
 
   const [currentpassword, setCurrentpassword] = useState('');
   const [newpassword, setNewpassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  
-  
+
+
   const handleSubmit = async (e) => {
-      e.preventDefault();
-      console.log({ currentpassword, newpassword, confirmPassword });
-    if(newpassword !==confirmPassword) {
-        alert('Passwords do not match!');
-        return;
+    e.preventDefault();
+    if (newpassword !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
     }
     // Handle form submission logic here
     try {
-        await axios.post(`/student/resetpassword`,
-            {
-                currentpassword,
-                newpassword,
-            }
-        )
-        alert('Password reset')
-        navigate('/student/dashboard')
+      await axios.post(`/${state && state.userType}/resetpassword`,
+        {
+          currentpassword,
+          newpassword,
+        }
+      )
+      alert('Password reset successfully')
+      navigate(`/${state && state.userType}/dashboard`)
     } catch (error) {
-        alert(error)
-        console.log(error)
+      alert(error.response.data.message)
+      console.log(error.response.data.message)
     }
   };
 
   return (
     info &&
     <div className="flex  flex-col items-center h-screen overflow-hidden bg-zinc-100">
-      <Nav keepactions={true} optionsAtRight={true} name={info.firstname} email = {info.email} img={info.avatar.url} />
+      <Nav keepactions={true} optionsAtRight={true} name={info.firstname} email={info.email} img={info.avatar ? info.avatar.url :info.organizationname.url} />
 
       <div className="w-full max-w-md p-8 my-auto space-y-6 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-semibold text-center text-gray-800">Change password</h2>
