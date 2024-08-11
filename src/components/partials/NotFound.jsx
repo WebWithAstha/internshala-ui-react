34c848813de3eb89b485e68d829adb29d02acea3
WebWithAstha <lodhiastha85@gmail.com>
2024-08-11 09:10:05 +0530

    not found page updated

--- src/components/partials/NotFound.jsx
+++ src/components/partials/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>Page Not Found.</div>
+    <div className='w-full h-screen flex justify-center flex-col items-center bg-sky-50'>
+      <img className='md:w-[50vw] w-[80vw] object-contain' draggable={false} src="https://static-00.iconduck.com/assets.00/404-page-not-found-illustration-512x249-ju1c9yxg.png" alt="" />
+      <h1 className='md:text-[2vw] text-cyan-700 mt-4 font-bold'>Oops.. Page Not Found</h1>
+    </div>
   )
 }
 
